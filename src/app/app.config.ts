ca549b0857e2224a9b658fd4b9233e485faa1ad5
omima <omimaelbanna14@gmail.com>
2025-10-16 00:13:18 +0300

    Deploy Angular app to GitHub Pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { BrowserAnimationsModule, provideAnimations } from "@angular/platform-browser/animations"
 import {CookieService} from 'ngx-cookie-service';
 import { provideToastr } from 'ngx-toastr';
@@ -11,20 +11,19 @@ import { errorInterceptor } from './core/interceptors/Error/error-interceptor';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { NgxPaginationModule } from 'ngx-pagination';
 import { loadingInterceptor } from './core/interceptors/loading-interceptor';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes , withViewTransitions(), withHashLocation()),
+    provideRouter(routes , withViewTransitions()),
     importProvidersFrom(BrowserAnimationsModule, NgxSpinnerModule, NgxPaginationModule ),
     provideHttpClient( withFetch() , withInterceptors([headerInterceptor , errorInterceptor , loadingInterceptor]) ),
     provideClientHydration(withEventReplay()),
     provideAnimations(),
     importProvidersFrom(CookieService),
     provideToastr(),
-    { provide: LocationStrategy, useClass: HashLocationStrategy },
 
   ]
 };
