f184e9a7152bfa79595d90af681ff5a063f4f3be
omima <omimaelbanna14@gmail.com>
2025-10-15 23:06:20 +0300

    Deploy Angular app to GitHub Pages

--- docs/environment.ts
+++ docs/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
     // baseURL ===> real (production) www.amazon.com
 
     baseURL : 'https://ecommerce.routemisr.com',
-    domain : 'http://localhost:4200'
+    // domain : 'http://localhost:4200'
+    domain : 'https://omimaelbanna14.github.io/Final-Project-Angular'
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,6 @@
 export const environment = {
     // baseURL ====> dev (test) www.test.amazon.com
     baseURL : 'https://ecommerce.routemisr.com',
-    // domain : 'http://localhost:4200'
-    domain : 'https://omimaelbanna14.github.io/Final-Project-Angular'
-
+    domain : 'http://localhost:4200'
+    
 };
